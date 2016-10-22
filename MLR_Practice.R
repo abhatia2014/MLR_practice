2c78ebc9d05501c4f2ab5b8c338070d0b911d5af
Aankur Bhatia <aank2007@gmail.com>
2016-10-22 18:49:42 -0400

    second commit

--- MLR_Practice.R
+++ MLR_Practice.R
@@ -36,3 +36,53 @@ r$aggr
 
 # Tutorial MLR ------------------------------------------------------------
 
+#Learning Tasks
+
+#RegrTask- for regression problems
+#ClassifTask- for classification problems
+#SurvTask- for survival task classification
+#ClusterTask- for cluster analysis
+#MultilabelTask- for Multilabel classification problems
+#CostSensTask- for cost sensitive classification
+
+
+# Regression Task Example -------------------------------------------------
+
+data(BostonHousing,package = "mlbench")
+head(BostonHousing)
+
+regr.task=makeRegrTask(id="bh",data=BostonHousing,target = "medv")
+regr.task
+
+
+# Classification Machine Learning Task ------------------------------------
+
+data(BreastCancer,package = "mlbench")
+head(BreastCancer)
+
+df=BreastCancer
+str(df)
+df$Id=NULL
+classiftask=makeClassifTask(id="bc",data=df,target = "Class")
+#in the makeClassifTask - the positive class can be set by positive="malignant"
+
+classiftask
+
+
+# Survival Analysis Tasks -------------------------------------------------
+
+#Survival tasks use two target columns
+
+install.packages("survival")
+library(survival)
+data(lung,package = "survival")
+head(lung)
+lung$status=(lung$status==2)#converts to logical
+
+surv.task=makeSurvTask(id="ls",data = lung,target = c("time","status"))
+surv.task
+
+
+# Multilabel Classification Tasks -----------------------------------------
+
+
