91429ddda7fba518157464cc8e2ba4223637f3ac
Aankur Bhatia <aank2007@gmail.com>
2016-10-23 14:29:26 -0400

    third commit

--- MLR_Practice.R
+++ MLR_Practice.R
@@ -85,4 +85,80 @@ surv.task
 
 # Multilabel Classification Tasks -----------------------------------------
 
+#in multilabel classification, each object can belong to more than one category
+#at one time
+
+#we use the 'yeast' dataset
+
+yeast=getTaskData(yeast.task)
+labels=colnames(yeast)[1:14]
+yeast.task=makeMultilabelTask(id="multi",data = yeast,target = labels)
+yeast.task
+
+
+# Cluster Analysis Task--------------------------------------------------------
+
+#we perform the Cluster Analysis Task using the MT Cars Data
+
+data("mtcars",package='datasets')
+head(mtcars)
+clustertask=makeClusterTask(data=mtcars)
+clustertask
+
+
+# Cost Sensitive Classification Task --------------------------------------
+
+library(dplyr)
+df=tbl_df(df)
+df
+iris=tbl_df(iris)
+iris
+?runif
+cost = matrix(runif(150 * 3, 0, 2000), 150) * (1 - diag(3))[iris$Species,]
+cost.sens.taks=makeCostSensTask(id="costsens",data=iris,cost=cost)
+cost.sens.taks
+
+#to get a task description, we use the following function
+
+getTaskDescription(classiftask)
+
+#to get the number of input variables
+
+getTaskNFeats(classiftask)
+
+#to get task levels
+
+getTaskClassLevels(classiftask)
+
+#get the names of input variables in a task
+
+getTaskFeatureNames(classiftask)
+getTaskFeatureNames(clustertask)
+
+#removing features with zero variance (constant features)
+
+removeConstantFeatures(clustertask)
+
+removeConstantFeatures(classiftask)
+
+#remove selected features
+#first get names
+getTaskFeatureNames(surv.task)
+
+dropFeatures(surv.task,c("meal.cal","wt.loss"))
+
+#standardize numerical features
+?normalizeFeatures
+
+task=normalizeFeatures(clustertask,method = "range")
+summary(getTaskData(task))
+
+
+# Constructing Learners ---------------------------------------------------
+
+#makelearner is to specify which learner (method) algorithm to use 
+
+#example classification trees 
+?makeLearner
+classif.learn=makeLearner('classif.randomForest',predict.type = "prob",fix.factors.prediction = TRUE)
 
